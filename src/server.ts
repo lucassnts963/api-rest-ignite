2012c69f97f091eb361c9261c92eaec74c8eeff8
Lucas Santos <lucassnts963@gmail.com>
2023-03-24 04:26:43 -0300

    sending the project

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from 'fastify'
+
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
+
+const app = fastify()
+
+app.register(transactionsRoutes)
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('listening on port 3333')
+  })
